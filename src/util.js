3031bac11d0fec941387d2f6747766d06155b245
deskach <dzianis.kachanau@gmail.com>
2017-09-13 08:45:15 +1000

    Implemented search for a flight.

--- src/util.js
+++ src/util.js
@@ -15,31 +15,55 @@ $(() => {
         return local.toJSON().slice(0, 10);
     }
 
+    function logError(msg) {
+        console.log(msg);
+    }
+
     _.set(window, 'locmoteFSI.api.initFlightSearchTemplate',
         (cssHook, context) => {
-            const DEFAULT_CONTEXT = {date: toDateInputValue(new Date()),};
+            const DEFAULT_CONTEXT = {
+                date: toDateInputValue(new Date()),
+                search_results: [],
+            };
+            const TEMPLATE_NAME = '#flight-search-template';
             const mergedContext = _.merge(context, DEFAULT_CONTEXT);
-            const $el = initHBTemplate('#flight-search-template', cssHook, mergedContext);
+            const $el = initHBTemplate(TEMPLATE_NAME, cssHook, mergedContext);
             const $form = $(`${cssHook} form[name="search"]`);
+            const proxyurl = "https://cors-anywhere.herokuapp.com/"; // Needed to get around CORS
 
-            $(`${cssHook} input[type="date"]`).val(mergedContext.date);
-            $form.on('submit', e => {
-                const proxyurl = "https://cors-anywhere.herokuapp.com/"; // Needed to get around CORS
+            function setSearchResults(data) {
+                _.assign(mergedContext, {search_results: data});
+                initHBTemplate(TEMPLATE_NAME, cssHook, mergedContext);
+                $form.css('cursor', 'default');
+
+                console.log(`Search succeeded for ${cssHook}`)
+            }
+
+            function search4Flights(success, failure, e) {
                 const url = proxyurl + 'http://node.locomote.com/code-task/flight_search/QF';
-                const args = 'date=2018-09-02&from=SYD&to=JFK';
+                const from = $(`${cssHook} input[name="from"]`).val();
+                const to = $(`${cssHook} input[name="toLoc"]`).val();
+                const when = $(`${cssHook} input[name="travelDate"]`).val().toString();
+                const args = `date=${when}&from=${from}&to=${to}`;
 
                 e.preventDefault();
 
                 $form.css('cursor', 'progress');
                 $.ajax({
                     url: `${url}?${args}`,
-                    success: (data) => {
-                        $form.css('cursor', 'default');
-                        console.log(JSON.stringify(data));
-                    },
+                    success: success,
+                    failure: failure,
                 });
-                // console.log(`Search was clicked on ${cssHook}`)
-            });
+            }
+
+            $form.on(
+                'submit',
+                search4Flights.bind(
+                    null,
+                    setSearchResults,
+                    logError.bind(null, "Failed to search for a flight")
+                )
+            );
 
             return $el;
         });
