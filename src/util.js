18c2bb2ca50ea9241cf4e0983544166ec340e57e
deskach <dzianis.kachanau@gmail.com>
2017-09-13 10:42:23 +1000

    Initial implementation of the front-end

--- src/util.js
+++ src/util.js
@@ -26,45 +26,91 @@ $(() => {
                 search_results: [],
             };
             const TEMPLATE_NAME = '#flight-search-template';
-            const mergedContext = _.merge(context, DEFAULT_CONTEXT);
+            let mergedContext = _.merge(context, DEFAULT_CONTEXT);
             const $el = initHBTemplate(TEMPLATE_NAME, cssHook, mergedContext);
             const $form = $(`${cssHook} form[name="search"]`);
             const proxyurl = "https://cors-anywhere.herokuapp.com/"; // Needed to get around CORS
 
-            function setSearchResults(data) {
-                _.assign(mergedContext, {search_results: data});
+            function resetSearchResults() {
+                mergedContext.search_results = [];
                 initHBTemplate(TEMPLATE_NAME, cssHook, mergedContext);
-                $form.css('cursor', 'default');
 
-                console.log(`Search succeeded for ${cssHook}`)
+                console.log(`Search results were resetted`)
             }
 
-            function search4Flights(success, failure, e) {
+            function setSearchResults(data) {
+                mergedContext.search_results = [...mergedContext.search_results, ...data];
+                initHBTemplate(TEMPLATE_NAME, cssHook, mergedContext);
+
+                console.log(`Search results were updated`)
+            }
+
+            function search4Flights(settings, success, failure) {
                 const url = proxyurl + 'http://node.locomote.com/code-task/flight_search/QF';
-                const from = $(`${cssHook} input[name="from"]`).val();
-                const to = $(`${cssHook} input[name="toLoc"]`).val();
-                const when = $(`${cssHook} input[name="travelDate"]`).val().toString();
+                const {from, to, when} = settings;
                 const args = `date=${when}&from=${from}&to=${to}`;
 
-                e.preventDefault();
-
-                $form.css('cursor', 'progress');
                 $.ajax({
-                    url: `${url}?${args}`,
+                    url: encodeURI(`${url}?${args}`),
                     success: success,
                     failure: failure,
                 });
             }
 
-            $form.on(
-                'submit',
-                search4Flights.bind(
-                    null,
-                    setSearchResults,
-                    logError.bind(null, "Failed to search for a flight")
-                )
-            );
+            function search4Airport(settings, success, failure) {
+                const {name} = settings;
+                const url = proxyurl + 'http://node.locomote.com/code-task/airports';
 
-            return $el;
+                $.ajax({
+                    url: encodeURI(`${url}?q=${name}`),
+                    success: success,
+                    failure: failure,
+                });
+            }
+
+            function searchAllFlights(settings) {
+                const {startingAirportCodes, endingAirportCodes, when} = settings;
+
+                resetSearchResults();
+
+                for (let code0 of startingAirportCodes) {
+                    for (let code1 of endingAirportCodes) {
+                        search4Flights(
+                            {from: code0, to: code1, when},
+                            data => setSearchResults(data),
+                            _ => logError(`Failed to find flights between ${code0} and ${code1}`),
+                        )
+                    }
+                }
+
+                console.log(startingAirportCodes, endingAirportCodes);
+            }
+
+            function onSubmit(e) {
+                const from = $(`${cssHook} input[name="from"]`).val();
+                const to = $(`${cssHook} input[name="toLoc"]`).val();
+                const when = $(`${cssHook} input[name="travelDate"]`).val().toString();
+
+                e.preventDefault();
+                $form.css('cursor', 'progress');
+
+                search4Airport(
+                    {name: from},
+                    startingAirports => search4Airport(
+                        {name: to},
+                        endingAirports => {
+                            searchAllFlights({
+                                startingAirportCodes: startingAirports.map(a => a.airportCode),
+                                endingAirportCodes: endingAirports.map(a => a.airportCode),
+                                when,
+                            });
+                        },
+                        _ => logError(`Failed to search for ${to}`)
+                    ),
+                    _ => logError(`Failed to search for ${from}`)
+                );
+            }
+
+            $form.on('submit', onSubmit);
         });
 });
