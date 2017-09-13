859578dc0577743d11a832fca65f22c491534537
deskach <dzianis.kachanau@gmail.com>
2017-09-14 05:30:26 +1000

    further enhancements to start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,4 @@ if [ ! -f ./server/package.json ] ; then
     git clone git@github.com:deskach/locmote-fsi.git -b gh-pages web
 fi
 
-cd ./server &&
-\ npm install &&
-\ npm run start
+cd ./server && npm install && npm start
