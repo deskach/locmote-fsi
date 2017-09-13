f74e47712e718bfd1d4d7e68ebedd90850a8ba74
deskach <dzianis.kachanau@gmail.com>
2017-09-13 17:00:21 +1000

    Now server serves the app on port 3000 :) also there is an /api endpoint which is not implemented yet.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/fsi', function (req, res) {
-    res.send('Hello FSI!')
+app.use('/', express.static('../web'));
+
+app.get('/api', function (req, res) {
+    res.send('API is not ready yet :(');
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-});
+// app.get('/', function (req, res) {
+//     res.send('Hello World!')
+// });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!');
 });
