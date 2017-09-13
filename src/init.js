4818a81445a2ca604627b862942fff86e913017d
deskach <dzianis.kachanau@gmail.com>
2017-09-13 13:47:05 +1000

    Moved front-end into web folder

--- src/init.js
+++
@@ -1,6 +0,0 @@
-$(() => {
-    const {initFlightSearchTemplate} = window.locmoteFSI.api;
-
-    initFlightSearchTemplate('.flight-to-form', {title: 'Starting point'});
-    initFlightSearchTemplate('.flight-from-form', {title: 'Destination'});
-});
