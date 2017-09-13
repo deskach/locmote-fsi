1cfa7223520310b2a41c1dbce50fd1db8500df59
deskach <dzianis.kachanau@gmail.com>
2017-09-14 06:26:11 +1000

    more enhancements to start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-if [ ! -f ./web/index.html ] ; then
+if [ ! -f "./web/index.html" ] ; then
+    echo "Cloning UI";
     git clone git@github.com:deskach/locmote-fsi.git -b gh-pages web
 fi
 
-cd ./server && npm install && npm start
+if [ ! -d "./server/node_modules" ] ; then
+    echo "Installing libraries...";
+    cd ./server && npm install && cd -
+fi
+
+cd ./server/ && npm start
